df2da1951f1e7e55e9782a512b937a44ee68d26a
Kurt Ball <kurtisdball@hotmail.com>
2021-05-23 15:32:02 -0600

    updated

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,20 +1,16 @@
 import { useState } from 'react'
 const Form = () => {
   const [recipient, setRecipient] = useState('')
-  const [email, setEmail] = useState('')
+  const [sender, setSender] = useState('')
   const [subject, setSubject] = useState('')
-  const [name, setName] = useState('')
-  const [message, setMessage] = useState('')
+  const [text, setText] = useState('')
 
-  const sendEmail = async () => {
-    await fetch(
-      `http://localhost:4000/send-email?recipient=${recipient}&sender=${email}&subject=${subject}&name=${name}&message=${message}`,
-    )
-      .then((res) => res.json())
-      .then((data) => console.log('sent success', data))
-      .catch((error) => {
-        console.log(`Error: ${error}`)
-      })
+  const sendEmail = () => {
+    fetch(
+      `http://localhost:4000/mail?recipient=${recipient}&sender=${sender}&topic=${subject}&text=${text}`,
+    ).catch((error) => {
+      console.log(`Error: ${error}`)
+    })
   }
 
   const submitHandler = (e) => {
@@ -24,7 +20,7 @@ const Form = () => {
 
   return (
     <form onSubmit={submitHandler}>
-      <div className="emailContainer">
+      <div classsubject="emailContainer">
         <label>RECIPIENT EMAIL:</label>
         <input
           onChange={(e) => {
@@ -34,7 +30,7 @@ const Form = () => {
         <label>EMAIL:</label>
         <input
           onChange={(e) => {
-            setEmail(e.target.value)
+            setSender(e.target.value)
           }}
         ></input>
         <label>SUBJECT:</label>
@@ -43,18 +39,13 @@ const Form = () => {
             setSubject(e.target.value)
           }}
         ></input>
-        <label>NAME:</label>
-        <input
-          onChange={(e) => {
-            setName(e.target.value)
-          }}
-        ></input>
         <label>MESSAGE:</label>
         <input
           onChange={(e) => {
-            setMessage(e.target.value)
+            setText(e.target.value)
           }}
         ></input>
+        <button type="submit">Submit</button>
       </div>
     </form>
   )
--- server/server.js
+++ server/server.js
@@ -1,8 +1,39 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const sgMail = require('@sendgrid/mail')
 const app = express()
 
 dotenv.config()
+app.use(cors())
+
+sgMail.setApiKey(process.env.API_KEY)
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/mail', (req, res) => {
+  const { recipient, sender, topic, text } = req.query
+  console.log('recipient', recipient, typeof recipient)
+  console.log('sender', sender)
+  console.log('subject', topic)
+  console.log('text', text)
+
+  const msg = {
+    to: recipient,
+    from: sender,
+    subject: topic,
+    text: text,
+  }
+  console.log('This is the msg', msg)
+  sgMail.send(msg, function (err, info) {
+    if (err) {
+      console.error('email not sent', err)
+    } else {
+      console.log(info, 'Success')
+    }
+  })
+})
+
 const port = process.env.PORT
 app.listen(port, () => console.log(`server is listening on port: ${port}`))
